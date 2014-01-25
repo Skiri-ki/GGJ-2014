a4c082931187863b1df7e6a3645eea7109ed10c7
Skiri-ki <git@skiri-ki.com>
2014-01-25 18:25:14 +0100

    ImprovedConnecting

--- Assets/Martin/Scripts/BodyPart.cs
+++ Assets/Martin/Scripts/BodyPart.cs
@@ -49,24 +49,58 @@ public abstract class BodyPart : Domain {
 		    (changedSideToY && Mathf.Abs(distBA.z) > Mathf.Abs(distBA.y))){
 			side = Vector3.forward * Mathf.Sign(distBA.z);
 		}
-		
-		Vector3 offsetToA = new Vector3(objA.collider.bounds.extents.x * -side.x, 
-		                                objA.collider.bounds.extents.y * -side.y,
-		                                objA.collider.bounds.extents.z * -side.z);
-		Vector3 offsetToB = new Vector3(objB.collider.bounds.extents.x * -side.x, 
-		                                objB.collider.bounds.extents.y * -side.y,
-		                                objB.collider.bounds.extents.z * -side.z);
-		Debug.Log(objA.transform.position + offsetToA + offsetToB);
-		objB.transform.position = objA.transform.position + offsetToA + offsetToB;
-		
-		Joint joint = objB.AddComponent<T>() as Joint;
-		joint.anchor = side *0.5f;
-		joint.axis =side;
 
 		Rigidbody rigidA = objA.rigidbody;
 		if(rigidA == null){
 			rigidA = objA.AddComponent<Rigidbody>();
 		}
+		
+		Rigidbody rigidB = objB.rigidbody;
+		if(rigidB == null){
+			rigidB = objB.AddComponent<Rigidbody>();
+		}
+
+		Vector3 offsetToA; 
+		if(objA.collider != null)
+			offsetToA = new Vector3(objA.collider.bounds.extents.x * -side.x, 
+			                        objA.collider.bounds.extents.y * -side.y,
+			                        objA.collider.bounds.extents.z * -side.z);
+		else if(objA.renderer != null)
+			offsetToA = new Vector3(objA.renderer.bounds.extents.x * -side.x, 
+			                        objA.renderer.bounds.extents.y * -side.y,
+			                        objA.renderer.bounds.extents.z * -side.z);
+		else{
+			offsetToA = rigidA.ClosestPointOnBounds(rigidB.transform.position) - rigidA.transform.position;
+			offsetToA = new Vector3(offsetToA.x * Mathf.Abs( side.x),
+			                        offsetToA.y * Mathf.Abs( side.y),
+			                        offsetToA.z * Mathf.Abs( side.z));
+			Debug.LogError("objA " + objA.name + " has neither renderer nor collider attached");
+		}
+
+		Vector3 offsetToB; 
+		
+		if(objB.collider != null)
+			offsetToB = new Vector3(objB.collider.bounds.extents.x * side.x, 
+			                        objB.collider.bounds.extents.y * side.y,
+			                        objB.collider.bounds.extents.z * side.z);
+		else if(objA.renderer != null)
+			offsetToB = new Vector3(objB.renderer.bounds.extents.x * side.x, 
+			                        objB.renderer.bounds.extents.y * side.y,
+			                        objB.renderer.bounds.extents.z * side.z);
+		else{
+			offsetToB = rigidB.ClosestPointOnBounds(rigidA.transform.position) - rigidB.transform.position ;
+			offsetToB = new Vector3(offsetToB.x * Mathf.Abs( side.x),
+			                        offsetToB.y * Mathf.Abs( side.y),
+			                        offsetToB.z * Mathf.Abs( side.z));
+			Debug.LogError("objA " + objA.name + " has neither renderer nor collider attached");
+		}
+
+		objB.transform.position = objA.transform.position + offsetToA - offsetToB;
+
+		Joint joint = objB.AddComponent<T>() as Joint;
+		joint.anchor = side *0.5f;
+		joint.axis = side;
+
 		
 		joint.connectedBody = rigidA;
 
